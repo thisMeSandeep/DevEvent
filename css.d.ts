92708d5189d92de406291f7bbe6c886778d520a5
Sandeep Singh <snayal50@gmail.com>
2025-10-25 19:10:40 +0530

    Project initiated

---
+++ css.d.ts
@@ -0,0 +1,3 @@
+//css d.ts
+
+declare module "*.css";
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
+  experimental: {
+    // Enable filesystem caching for `next dev`
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
